eda0b475f1ff10facc1cf95ab0418aaf6c33cb48
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-07 19:18:50 +0800

    Displaying versions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const { dbConnect } = require("./v1/config/mongodb");
 const v1Routes = require("./v1/routes");
+const { versions } = require("./variables");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/v1", v1Routes);
 
+const displayAppVersions = () => {
+  console.log("API VERSIONS: ");
+  for (let i = 0; i < versions.length; i++) {
+    console.log(`- ${versions[i]}`);
+  }
+};
+
 app.listen(PORT, () => {
   console.log("SERVER UP -> PORT:", PORT);
+  displayAppVersions();
   dbConnect("todo");
 });
---
+++ src/variables.js
@@ -0,0 +1,3 @@
+module.exports = {
+  versions: ["v1"],
+};
