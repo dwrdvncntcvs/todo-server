f134507b377d33543ded49581bf43ebf207a3668
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-11-10 18:53:15 +0800

    User routes intialized

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const task = require("./task");
-const subtasks = require("./subtask");
+const subtask = require("./subtask");
+const user = require("./user");
 
 const routes = express.Router();
 
 routes.use("/task", task);
-routes.use("/subtask", subtasks);
+routes.use("/subtask", subtask);
+routes.use("/user", user);
 
 module.exports = routes;
